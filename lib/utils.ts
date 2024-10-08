b829eeb136ec9c5e94e2c9a18da0ac451d36cdcb
kadeem-lewis <email@kadeemlewis.com>
2024-10-08 04:51:55 -0400

    updated cn util to use clsx function from cva instead of separate package

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { cx, type CxOptions } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: CxOptions) {
+  return twMerge(cx(inputs));
 }
